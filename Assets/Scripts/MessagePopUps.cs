522ea9aac0ddecfb3cf52b0030a3329280a8e95e
andjela31 <andjeladjo@gmail.com>
2026-02-28 18:52:52 +0100

    Mala izmena

--- Assets/Scripts/MessagePopUps.cs
+++ Assets/Scripts/MessagePopUps.cs
@@ -19,10 +19,21 @@ public class MessagePopUps : MonoBehaviour
         string text = msg.Replace("SRV:", "");
         string header = "Message from server:";
         Transform panelInstance = Instantiate(panelPrefab, remote);
+
+        RectTransform remoteRT = remote.GetComponent<RectTransform>();
+
+        LayoutElement le = msgPrefab.GetComponent<LayoutElement>();
+        if (le != null)
+        {
+            le.preferredWidth = remoteRT.rect.width * 0.7f;
+        }
+
         TMP_Text headerInstance = Instantiate(msgPrefab, panelInstance);
         headerInstance.text = header;
         TMP_Text textInstance = Instantiate(msgPrefab, panelInstance);
         textInstance.text = text;
+
+
         LayoutRebuilder.ForceRebuildLayoutImmediate(panelInstance.GetComponent<RectTransform>());
 
         ThemeManager.Instance.ApplyActiveThemeToMessagePanel(panelInstance);
